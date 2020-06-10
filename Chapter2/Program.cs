6922f3e58d95ec788c390a20b9ce2a18fdacb8fe
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2020-06-10 23:51:10 +0100

    First Exercise done

---
+++ Chapter2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace Chapter2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please Enter two numbers.");
+            Console.WriteLine("--------------------------");
+            
+            Console.WriteLine("Enter First Number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Second NUmber: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int sum = firstNumber + secondNumber;
+            int product = firstNumber * secondNumber;
+            int difference = firstNumber / secondNumber;
+            int quotient = firstNumber % secondNumber;
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("RESULTS\n=========");
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Product: {product}");
+            Console.WriteLine($"Difference: {difference}");
+            Console.WriteLine($"Modulus: {quotient}");
+
+        }
+
+
+    }
+}
